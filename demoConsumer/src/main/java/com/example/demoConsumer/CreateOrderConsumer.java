223d222a305efbba59ff4d8b2abfb26cd648a3d7
Harry Kimpel <harrykimpel@hotmail.com>
2024-02-13 09:12:25 +0100

    minor updates for testing purposes
    Signed-off-by: Harry Kimpel <harrykimpel@hotmail.com>

--- demoConsumer/src/main/java/com/example/demoConsumer/CreateOrderConsumer.java
+++ demoConsumer/src/main/java/com/example/demoConsumer/CreateOrderConsumer.java
@@ -6,6 +6,10 @@ import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Scope;
+import io.opentelemetry.instrumentation.annotations.SpanAttribute;
+import io.opentelemetry.instrumentation.annotations.WithSpan;
+
+import java.security.SecureRandom;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +18,7 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service("orderConsumerNotificationService")
 public class CreateOrderConsumer {
@@ -30,6 +35,49 @@ public class CreateOrderConsumer {
         try (Scope scope = span.makeCurrent()) {
             log.info("Notification service received order {} ", order);
             ack.acknowledge();
+
+            Integer secondsToSleep = 3;
+            ExecuteLongrunningTask(secondsToSleep);
+
+            SecureRandom secureRandom = new SecureRandom();
+            int randomWithSecureRandom = secureRandom.nextInt(10);
+            log.info("randomWithSecureRandom: " + randomWithSecureRandom);
+            GetUser(randomWithSecureRandom);
+        } catch (Throwable t) {
+            span.recordException(t);
+            throw t;
+        } finally {
+            span.end();
+        }
+    }
+
+    private void ExecuteLongrunningTask(Integer secondsToSleep) {
+        Span span = tracer.spanBuilder("ExecuteLongrunningTask").startSpan();
+        span.setAttribute("secondsToSleep", secondsToSleep);
+        // Make the span the current span
+        try (Scope scope = span.makeCurrent()) {
+            Thread.sleep(secondsToSleep * 1000);
+            log.info("Executed some long running task that took " + secondsToSleep + " seconds to run.");
+        } catch (Exception t) {
+            span.recordException(t);
+            // throw t;
+        } finally {
+            span.end();
+        }
+    }
+
+    private void GetUser(Integer randomUser) {
+        Span span = tracer.spanBuilder("GetUser").startSpan();
+        // Make the span the current span
+        try (Scope scope = span.makeCurrent()) {
+            String uri = "https://jsonplaceholder.typicode.com/users/" + randomUser;
+            span.setAttribute("uri", uri);
+            RestTemplate restTemplate = new RestTemplate();
+
+            User user = restTemplate.getForObject(uri, User.class);
+            log.info("User: " + user);
+            log.info("User id: " + user.getId());
+            span.setAttribute("user.id", user.getId());
         } catch (Throwable t) {
             span.recordException(t);
             throw t;
