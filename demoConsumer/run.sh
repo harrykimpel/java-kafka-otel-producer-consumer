3062aaeae19d3cdc2a38d2b774c77700653db7d2
Harry Kimpel <harrykimpel@hotmail.com>
2024-02-14 15:45:32 +0100

    Adding in another service
    Signed-off-by: Harry Kimpel <harrykimpel@hotmail.com>

--- demoConsumer/run.sh
+++ demoConsumer/run.sh
@@ -2,8 +2,10 @@ export JAVA_TOOL_OPTIONS="-javaagent:/Users/hkimpel/projects/kafka/java/opentele
 export OTEL_TRACES_EXPORTER=otlp
 export OTEL_METRICS_EXPORTER=otlp
 export OTEL_LOGS_EXPORTER=otlp
-export OTEL_EXPORTER_OTLP_ENDPOINT='https://otlp.nr-data.net'           # US region
-#export OTEL_EXPORTER_OTLP_ENDPOINT='https://otlp.eu01.nr-data.net'     # EU region
+# US region
+export OTEL_EXPORTER_OTLP_ENDPOINT='https://otlp.nr-data.net'
+# EU region
+#export OTEL_EXPORTER_OTLP_ENDPOINT='https://otlp.eu01.nr-data.net'
 export OTEL_EXPORTER_OTLP_HEADERS="api-key=NEW_RELIC_LICENSE_KEY"
 export OTEL_SERVICE_NAME="kafka-java-consumer"
 export OTEL_SERVICE_VERSION="0.1.0"
