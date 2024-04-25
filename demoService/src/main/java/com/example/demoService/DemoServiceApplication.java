b149c5db80d5ade489504e2680ace3d442b43bd2
Harry Kimpel <harrykimpel@hotmail.com>
2024-04-25 08:19:11 +0200

    Fixing the context propagation
    Signed-off-by: Harry Kimpel <harrykimpel@hotmail.com>

--- demoService/src/main/java/com/example/demoService/DemoServiceApplication.java
+++ demoService/src/main/java/com/example/demoService/DemoServiceApplication.java
@@ -1,12 +1,12 @@
 package com.example.demoService;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.baggage.propagation.W3CBaggagePropagator;
 import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
 import io.opentelemetry.context.propagation.ContextPropagators;
 import io.opentelemetry.exporter.otlp.logs.OtlpGrpcLogRecordExporter;
@@ -21,6 +21,7 @@ import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
 import io.opentelemetry.semconv.ResourceAttributes;
+import io.opentelemetry.context.propagation.TextMapPropagator;
 
 @SpringBootApplication
 public class DemoServiceApplication {
@@ -31,6 +32,7 @@ public class DemoServiceApplication {
 	private String otlpHeadersApiKey;
 
 	public static void main(String[] args) {
+		System.setProperty("otel.jmx.target.system", "tomcat");
 		SpringApplication.run(DemoServiceApplication.class, args);
 	}
 
@@ -38,15 +40,20 @@ public class DemoServiceApplication {
 	public OpenTelemetry openTelemetry() {
 		Resource resource = Resource.getDefault().toBuilder()
 				.put(ResourceAttributes.SERVICE_NAME, "kafka-java-service")
-				.put(ResourceAttributes.SERVICE_VERSION, "0.1.0").build();
+				.put(ResourceAttributes.SERVICE_VERSION, "0.1.0")
+				.put("otel.jmx.target.system", "tomcat")
+				.build();
 
 		SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
+
 				.addSpanProcessor(BatchSpanProcessor.builder(OtlpGrpcSpanExporter.builder()
 						.setEndpoint(
 								otlpEndpoint)
 						.addHeader("api-key",
 								otlpHeadersApiKey)
 						.build()).build())
+
+				// .addSpanProcessor(SimpleSpanProcessor.create(LoggingSpanExporter.create()))
 				.setResource(resource)
 				.build();
 
@@ -75,7 +82,10 @@ public class DemoServiceApplication {
 				.setTracerProvider(sdkTracerProvider)
 				.setMeterProvider(sdkMeterProvider)
 				.setLoggerProvider(sdkLoggerProvider)
-				.setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
+				// .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
+				.setPropagators(ContextPropagators.create(TextMapPropagator
+						.composite(W3CTraceContextPropagator.getInstance(), W3CBaggagePropagator.getInstance())))
+
 				.buildAndRegisterGlobal();
 
 		return openTelemetry;
--- demoService/src/main/java/com/example/demoService/HelloController.java
+++ demoService/src/main/java/com/example/demoService/HelloController.java
@@ -3,40 +3,83 @@ package com.example.demoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Scope;
+import io.opentelemetry.context.propagation.TextMapGetter;
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 public class HelloController {
 
     private static final Logger log = LoggerFactory.getLogger(HelloController.class);
     private final Tracer tracer;
+    private final OpenTelemetry openTelemetry;
+    @Autowired
+    private HttpServletRequest request;
+
+    TextMapGetter<HttpServletRequest> getter = new TextMapGetter<HttpServletRequest>() {
+        @Override
+        public String get(HttpServletRequest carrier, String key) {
+            if (carrier.getHeader(key) != null) {
+                return carrier.getHeader(key);
+            }
+            return null;
+        }
+
+        @Override
+        public Iterable<String> keys(HttpServletRequest carrier) {
+            // return carrier.getHeaderNames();
+            return java.util.Collections.list(carrier.getHeaderNames());
+        }
+    };
 
     @GetMapping("/")
     public String index() {
-        Span span = tracer.spanBuilder("greetFromHomeController").startSpan();
 
-        // Make the span the current span
-        try (Scope scope = span.makeCurrent()) {
-            String msg = "Greetings from Spring Boot service!";
-            log.info("return message: " + msg);
-            return msg;
-        } catch (Throwable t) {
-            span.recordException(t);
-            throw t;
-        } finally {
-            span.end();
+        // Extract the SpanContext and other elements from the request.
+        io.opentelemetry.context.Context extractedContext = openTelemetry.getPropagators()
+                .getTextMapPropagator().extract(
+                        io.opentelemetry.context.Context.root(),
+                        request,
+                        getter);
+
+        try (Scope scope = extractedContext.makeCurrent()) {
+            // Automatically use the extracted SpanContext as parent.
+            Span serverSpan = tracer.spanBuilder("get_demoService")
+                    .setSpanKind(SpanKind.SERVER)
+                    .startSpan();
+
+            try {
+                // Add the attributes defined in the Semantic Conventions
+                serverSpan.setAttribute("http.method", "GET");
+                serverSpan.setAttribute("http.scheme", "http");
+                serverSpan.setAttribute("http.host", "localhost:8082");
+                serverSpan.setAttribute("http.target", "/");
+
+                String msg = "Greetings from Spring Boot service!";
+                log.info("return message: " + msg);
+                return msg;
+                /*
+                 * } catch (Throwable t) {
+                 * remoteContext.recordException(t);
+                 * throw t;
+                 */
+            } finally {
+                serverSpan.end();
+            }
         }
     }
 
     @Autowired
     HelloController(OpenTelemetry openTelemetry) {
+        this.openTelemetry = openTelemetry;
+
         tracer = openTelemetry.getTracer(DemoServiceApplication.class.getName(), "0.1.0");
     }
 }
