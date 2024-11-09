e9fde44f52e2089315da314a1d999f1fc9acdbfb
Harry Kimpel <harrykimpel@hotmail.com>
2024-11-09 17:17:49 +0000

    Minor updates
    Signed-off-by: Harry Kimpel <harrykimpel@hotmail.com>

--- demoConsumer/src/main/java/com/example/demoConsumer/CreateOrderConsumer.java
+++ demoConsumer/src/main/java/com/example/demoConsumer/CreateOrderConsumer.java
@@ -2,6 +2,11 @@ package com.example.demoConsumer;
 
 import com.example.demoProducer.Order;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
 import java.security.SecureRandom;
 
 import org.slf4j.Logger;
@@ -21,6 +26,10 @@ public class CreateOrderConsumer {
     public void createOrderListener(@Payload Order order, Acknowledgment ack) {
         log.info("Notification service received order {} ", order);
         ack.acknowledge();
+
+        CallDemoService1();
+
+        CallDemoService2();
 
         Integer secondsToSleep = 3;
         ExecuteLongrunningTask(secondsToSleep);
@@ -47,5 +56,44 @@ public class CreateOrderConsumer {
         User user = restTemplate.getForObject(uri, User.class);
         log.info("User: " + user);
         log.info("User id: " + user.getId());
+    }
+
+    private void CallDemoService1() {
+        try {
+            String uri = "http://localhost:8082";
+            String userAgent = "java.net.HttpURLConnection";
+
+            URL obj = URI.create(uri).toURL();
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", userAgent);
+            int responseCode = con.getResponseCode();
+            System.out.println("GET Response Code :: " + responseCode);
+            if (responseCode == HttpURLConnection.HTTP_OK) { // success
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                // print result
+                log.info("Demo Service response: " + response.toString());
+            } else {
+                log.info("GET request did not work.");
+            }
+        } catch (Exception ex) {
+
+        }
+    }
+
+    private void CallDemoService2() {
+        String uri = "http://localhost:8082";
+        RestTemplate restTemplate = new RestTemplate();
+        String resp = restTemplate.getForObject(uri, String.class);
+
+        log.info("Demo Service response: " + resp);
     }
 }
