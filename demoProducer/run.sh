5730bfae6ee3953fd8edf110161def558a285928
Harry Kimpel <harrykimpel@hotmail.com>
2024-12-04 17:08:57 +0100

    Adding Google Pub-Sub version
    Signed-off-by: Harry Kimpel <harrykimpel@hotmail.com>

--- demoProducer/run.sh
+++ demoProducer/run.sh
@@ -1,1 +1,13 @@
+export JAVA_TOOL_OPTIONS="-javaagent:/Users/hkimpel/projects/kafka/java-local-test/otel-auto-instr/opentelemetry-javaagent.jar"
+export OTEL_TRACES_EXPORTER=otlp
+export OTEL_METRICS_EXPORTER=otlp
+export OTEL_LOGS_EXPORTER=otlp
+# US region
+export OTEL_EXPORTER_OTLP_ENDPOINT='https://otlp.nr-data.net'
+# EU region
+#export OTEL_EXPORTER_OTLP_ENDPOINT='https://otlp.eu01.nr-data.net'
+export OTEL_EXPORTER_OTLP_HEADERS="api-key=NEW_RELIC_LICENSE_KEY"
+export OTEL_SERVICE_NAME="kafka-java-producer"
+
 ./mvnw spring-boot:run
+
