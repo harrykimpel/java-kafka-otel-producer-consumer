cd8ae272671527533075e3695808529e99b9ed12
Harry Kimpel <harrykimpel@hotmail.com>
2025-01-28 16:20:43 +0100

    Updating Tracing Kafka with OTel
    Signed-off-by: Harry Kimpel <harrykimpel@hotmail.com>

---
+++ demoProducer/src/main/java/com/example/demoProducer/UserConsumer.java
@@ -0,0 +1,41 @@
+package com.example.demoProducer;
+
+import com.example.demoProducer.Order;
+import com.example.demoConsumer.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.security.SecureRandom;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import java.util.concurrent.ExecutionException;
+
+@Service("userConsumerNotificationService")
+public class UserConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(UserConsumer.class);
+
+    @KafkaListener(topics = "${spring.kafka.order.topic.user}", containerFactory = "containerFactoryNotificationService")
+    public void userListener(@Payload User user, Acknowledgment ack) {
+        log.info("Notification service received user {} ", user);
+        ack.acknowledge();
+
+
+    }
+
+
+}
