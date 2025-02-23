c9ee6431f863cbbcea7c446c33cdffe2625c6871
Harry Kimpel <harrykimpel@hotmail.com>
2025-02-23 19:39:16 +0100

    Update run.sh

--- demoConsumer/run.sh
+++ demoConsumer/run.sh
@@ -1,4 +1,4 @@
-export JAVA_TOOL_OPTIONS="-javaagent:/Users/hkimpel/projects/kafka/java/opentelemetry-javaagent.jar"
+export JAVA_TOOL_OPTIONS="-javaagent:/root/java-kafka-otel-producer-consumer/opentelemetry-javaagent.jar"
 export OTEL_TRACES_EXPORTER=otlp
 export OTEL_METRICS_EXPORTER=otlp
 export OTEL_LOGS_EXPORTER=otlp
@@ -6,7 +6,7 @@ export OTEL_LOGS_EXPORTER=otlp
 export OTEL_EXPORTER_OTLP_ENDPOINT='https://otlp.nr-data.net'
 # EU region
 #export OTEL_EXPORTER_OTLP_ENDPOINT='https://otlp.eu01.nr-data.net'
-export OTEL_EXPORTER_OTLP_HEADERS="api-key=NEW_RELIC_LICENSE_KEY"
+export OTEL_EXPORTER_OTLP_HEADERS="api-key=${NEW_RELIC_LICENSE_KEY}"
 export OTEL_SERVICE_NAME="kafka-java-consumer"
 export OTEL_SERVICE_VERSION="0.1.0"
 
