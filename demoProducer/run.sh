906ef60e7560797e50355d09a525c634b77c6e2f
Harry Kimpel <harrykimpel@hotmail.com>
2025-02-23 19:39:39 +0100

    Update run.sh

--- demoProducer/run.sh
+++ demoProducer/run.sh
@@ -7,7 +7,7 @@ export OTEL_EXPORTER_OTLP_ENDPOINT='https://otlp.nr-data.net'
 # EU region
 #export OTEL_EXPORTER_OTLP_ENDPOINT='https://otlp.eu01.nr-data.net'
 export OTEL_EXPORTER_OTLP_HEADERS="api-key=${NEW_RELIC_LICENSE_KEY}"
-export OTEL_SERVICE_NAME="kafka-java-consumer"
+export OTEL_SERVICE_NAME="kafka-java-producer"
 export OTEL_SERVICE_VERSION="0.1.0"
 
 ./mvnw spring-boot:run
